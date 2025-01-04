d2f148b460c0c4e8d4b7067d3b27fc5a7091ab56
gustavogss <gustavogss.dev@gmail.com>
2025-01-04 15:59:50 -0300

    feat: create side bar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function DashboardPage() {
   return (
     <div className="">
       <p>Dashboard</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   )
 
