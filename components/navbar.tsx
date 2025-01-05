77a601ccc8fcb7fc17a3bd3fd58c628b6207d138
gustavogss <gustavogss.dev@gmail.com>
2025-01-04 22:42:13 -0300

    feat: create dashboard

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,7 @@ import { UserButton } from "@clerk/nextjs";
 import MobileSidebar from "@/components/mobile-sidebar";
 
 export default function NavBar() {
+
   return (
     <div className="flex items-center p-4">
       <MobileSidebar />
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 
 const montserrat = Montserrat({
@@ -28,7 +29,7 @@ const routes = [
     label: "Image Generation",
     icon: ImageIcon,
     href: "/image",
-    color: "text-pink-700"
+    color: "text-orange-400"
   },
   {
     label: "Video Generation",
@@ -56,6 +57,7 @@ const routes = [
 ]
 
 export default function SideBar() {
+  const pathName = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#212121] text-slate-50">
       <div className="px-3 py-2 flex-1">
@@ -76,8 +78,9 @@ export default function SideBar() {
             <Link
               href={route.href}
               key={route.href}
-              className="text-md group flex p-3 w-full justify-start font-medium cursor-pointer 
-              hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn("text-md group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathName === route.href ? "text-white bg-white/10" : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1 pl-4">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
